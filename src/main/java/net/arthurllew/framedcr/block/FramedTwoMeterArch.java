642bed162d76960ae926a8498ac89caa0908ec5e
ArthurLlew <non.fatum.mori@gmail.com>
2026-01-09 17:32:01 +0300

    Code refactor

--- src/main/java/net/arthurllew/framedcr/block/FramedTwoMeterArch.java
+++ src/main/java/net/arthurllew/framedcr/block/FramedTwoMeterArch.java
@@ -7,7 +7,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
